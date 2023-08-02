6047c28cb9bc668d7330c80956614791515ec33f
Lizaveta <lizadondysh@icloud.com>
2023-08-02 23:58:29 +0300

    feat: amend jest config to provide info about test coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
