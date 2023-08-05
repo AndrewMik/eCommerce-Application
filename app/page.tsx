e151bef601c218314e2be2155b7798b5b1cc18da
Alina Nosovets <alinanosovets6849@gmail.com>
2023-08-05 23:32:30 +0200

    build: add ant design

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,13 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import { Button, Space, DatePicker, version } from 'antd';
 
-export default function Home(): JSX.Element {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
-          </a>
-        </div>
-      </div>
+const Page = (): JSX.Element => (
+  <div style={{ padding: '0 24px' }}>
+    <h1>antd version: {version}</h1>
+    <Space>
+      <DatePicker />
+      <Button type="primary">Primary Button</Button>
+    </Space>
+  </div>
+);
 
-      <div className={styles.center}>
-        <Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-      </div>
-    </main>
-  );
-}
+export default Page;
