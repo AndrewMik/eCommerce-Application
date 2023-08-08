9d26d40fdf0020ea76a8896c9275787d617a548e
Lizaveta <lizadondysh@icloud.com>
2023-08-09 02:32:59 +0300

    feat: add basic client

---
+++ api/types.ts
@@ -0,0 +1,11 @@
+enum ClientEnvData {
+  KEY = 'CTP_PROJECT_KEY',
+  ID = 'CTP_CLIENT_ID',
+  SECRET = 'CTP_CLIENT_SECRET',
+  AUTH_URL = 'CTP_AUTH_URL',
+  API_URL = 'CTP_API_URL',
+  SCOPES = 'CTP_SCOPES',
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { ClientEnvData };
