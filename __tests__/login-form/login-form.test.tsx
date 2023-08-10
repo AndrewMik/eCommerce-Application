138058e340033199ddb02ac37b8ec7456c22bd0c
Andrew Mikitchuk <andrew.mikitchuk@gmail.com>
2023-08-10 14:34:38 +0400

    refactor: rewrite similar tests using test.each

--- __tests__/login-form/login-form.test.tsx
+++ __tests__/login-form/login-form.test.tsx
@@ -4,6 +4,8 @@ import mockMatchMedia from '../../utils/mock';
 import LoginForm from '../../components/login-form/login-form';
 import { Placeholders, ValidationMessages } from '../../components/login-form/enums.login-form';
 
+const formatMessage = (message: string): string => message.replace(/\s\s+/g, ' ');
+
 mockMatchMedia();
 
 enum TestMessage {
@@ -47,42 +49,16 @@ describe('LoginForm - email validation', () => {
     });
   });
 
-  it(`${TestMessage.InvalidEmail} with leading whitespace`, async () => {
+  test.each([
+    [`${TestMessage.InvalidEmail} with leading whitespace`, ' name@gmail.com'],
+    [`${TestMessage.InvalidEmail} with trailing whitespace`, 'name@gmail.com '],
+    [`${TestMessage.InvalidEmail} without @`, 'namegmail.com'],
+    [`${TestMessage.InvalidEmail} with two @`, 'na@me@gmail.com'],
+    [`${TestMessage.InvalidEmail} without domain`, 'name@gmail'],
+  ])('%s - "%s"', async (_, email) => {
     render(<LoginForm />);
 
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Email), { target: { value: ' name@gmail.com' } });
-
-    expect(await screen.findByText(ValidationMessages.EmailInvalid)).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidEmail} with trailing whitespace`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Email), { target: { value: 'name@gmail.com ' } });
-
-    expect(await screen.findByText(ValidationMessages.EmailInvalid)).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidEmail} without @`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Email), { target: { value: 'namegmail.com' } });
-
-    expect(await screen.findByText(ValidationMessages.EmailInvalid)).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidEmail} with two @`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Email), { target: { value: 'na@me@gmail.com' } });
-
-    expect(await screen.findByText(ValidationMessages.EmailInvalid)).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidEmail} without domain`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Email), { target: { value: 'name@gmail' } });
+    fireEvent.change(screen.getByPlaceholderText(Placeholders.Email), { target: { value: email } });
 
     expect(await screen.findByText(ValidationMessages.EmailInvalid)).toBeInTheDocument();
   });
@@ -95,69 +71,29 @@ describe('LoginForm - password validation', () => {
     fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: '12345sS' } });
 
     await waitFor(() => {
-      expect(screen.getByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).toBeInTheDocument();
+      expect(screen.getByText(formatMessage(ValidationMessages.PasswordPattern))).toBeInTheDocument();
     });
 
     fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: '12345sS!' } });
 
     await waitFor(() => {
-      expect(screen.queryByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).not.toBeInTheDocument();
+      expect(screen.queryByText(formatMessage(ValidationMessages.PasswordPattern))).not.toBeInTheDocument();
     });
   });
 
-  it(`${TestMessage.InvalidPassword} if less than 8 characters were used`, async () => {
+  test.each([
+    [`${TestMessage.InvalidPassword} if less than 8 characters were used`, '1234sS!'],
+    [`${TestMessage.InvalidPassword} with leading whitespace`, ' sS1!sS1!'],
+    [`${TestMessage.InvalidPassword} with trailing whitespace`, 'sS1!sS1! '],
+    [`${TestMessage.InvalidPassword} without lowercase character`, 'SS1!SS1!S'],
+    [`${TestMessage.InvalidPassword} without uppercase character`, 'ss1!ss1!s'],
+    [`${TestMessage.InvalidPassword} without digit character`, 'ssS!ssS!s'],
+    [`${TestMessage.InvalidPassword} without special character`, 'ssSsssS1s'],
+  ])('%s - "%s"', async (_, password) => {
     render(<LoginForm />);
 
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: '1234sS!' } });
+    fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: password } });
 
-    expect(await screen.findByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidPassword} with leading whitespace`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: ' sS1!sS1!' } });
-
-    expect(await screen.findByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidPassword} with trailing whitespace`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: 'sS1!sS1! ' } });
-
-    expect(await screen.findByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidPassword} without lowercase character`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: 'SS1!SS1!S' } });
-
-    expect(await screen.findByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidPassword} without uppercase character`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: 'ss1!ss1!s' } });
-
-    expect(await screen.findByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidPassword} without digit character`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: 'ssS!ssS!s' } });
-
-    expect(await screen.findByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).toBeInTheDocument();
-  });
-
-  it(`${TestMessage.InvalidPassword} without special character`, async () => {
-    render(<LoginForm />);
-
-    fireEvent.change(screen.getByPlaceholderText(Placeholders.Password), { target: { value: 'ssSsssS1s' } });
-
-    expect(await screen.findByText(ValidationMessages.PasswordPattern.replace(/\s\s+/g, ' '))).toBeInTheDocument();
+    expect(await screen.findByText(formatMessage(ValidationMessages.PasswordPattern))).toBeInTheDocument();
   });
 });
