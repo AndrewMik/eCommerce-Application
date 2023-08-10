ab979920d0204bd9d54c5ca9093723d28a61d9c9
Alina Nosovets <alinanosovets6849@gmail.com>
2023-08-10 21:28:37 +0200

    feat: make navigation work properly on forward/back browser buttons and on page reload

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,6 +3,7 @@
 import { Layout, Menu, Col, Row } from 'antd';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 import logo from '../../public/kiddo-kingdom-logo.svg';
 
 const MainHeader: React.FC = () => {
@@ -14,6 +15,31 @@ const MainHeader: React.FC = () => {
     { key: '/catalog', label: 'Catalog' },
     { key: '/cart', label: 'Cart' },
   ];
+
+  const getInitialSelectedItemKey = () => {
+    const currentPath = window.location.pathname;
+    return `item${menuItems.findIndex((item) => item.key === currentPath)}`;
+  };
+
+  const [selectedNavLink, setSelectedNavLink] = useState<string>(getInitialSelectedItemKey);
+
+  useEffect(() => {
+    const updateSelectedNavKey = () => {
+      setSelectedNavLink(getInitialSelectedItemKey());
+    };
+
+    window.addEventListener('popstate', updateSelectedNavKey);
+    return () => {
+      window.removeEventListener('popstate', updateSelectedNavKey);
+    };
+  }, []);
+
+  const handleNavClick = (path: string) => {
+    setTimeout(() => {
+      const key = `item${menuItems.findIndex((item) => item.key === path)}`;
+      setSelectedNavLink(key);
+    }, 0);
+  };
 
   return (
     <>
@@ -31,16 +57,28 @@ const MainHeader: React.FC = () => {
           <Row>
             <Col span={8}>
               <Link href={'./'}>
-                <Image src={logo} width={120} height={80} alt="Picture of the author" style={{ marginRight: '20px' }} />
+                <Image
+                  src={logo}
+                  width={120}
+                  height={80}
+                  alt="Picture of the author"
+                  style={{ marginRight: '20px' }}
+                  priority={true}
+                />
               </Link>
             </Col>
             <Col span={8} offset={8}>
               <Menu
                 mode="horizontal"
                 style={{ backgroundColor: '#f5f5f5' }}
+                selectedKeys={[selectedNavLink]}
                 items={menuItems.map((item, index) => ({
                   key: `item${index}`,
-                  label: <Link href={item.key}>{item.label}</Link>,
+                  label: (
+                    <Link href={item.key} onClick={() => handleNavClick(item.key)}>
+                      {item.label}
+                    </Link>
+                  ),
                 }))}
               />
             </Col>
