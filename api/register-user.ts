31b31e95b2dc699a6ecb8700b6e9dba88f4fecd3
Andrew Mikitchuk <andrew.mikitchuk@gmail.com>
2023-08-18 10:37:03 +0400

    refactor: fix code styling

--- api/register-user.ts
+++ api/register-user.ts
@@ -1,12 +1,24 @@
 import { CustomerDraft } from '@commercetools/platform-sdk';
-import Client from './client';
 import { getCode } from 'country-list';
-import {
-  ShippingFieldsName,
-  BillingFieldsName,
-  FormData,
-} from '../components/registration-form/helpers/registration.types';
+import Client from './client';
+import { AddressFieldsName, FormData } from '../components/registration-form/helpers/registration.types';
 import { getSalutation, getBirthDate } from '../components/registration-form/helpers/helper-functions';
+
+const getDefaultBillingAddress = (formData: FormData) => {
+  if (formData.useAsBillingAddress) {
+    return formData.setAsDefault_shipping ? 0 : undefined;
+  }
+
+  return formData.setAsDefault_billing ? 1 : undefined;
+};
+
+const createAddress = (formData: FormData, type: 'shipping' | 'billing') => ({
+  country: getCode(formData[`${AddressFieldsName.COUNTRY}_${type}`]) as string,
+  streetName: formData[`${AddressFieldsName.STREET_NAME}_${type}`],
+  streetNumber: formData[`${AddressFieldsName.STREET_NUMBER}_${type}`],
+  apartment: formData[`${AddressFieldsName.APARTMENT}_${type}`],
+  postalCode: formData[`${AddressFieldsName.POSTAL_CODE}_${type}`],
+});
 
 async function registerUser(formData: FormData) {
   const customerDraft: CustomerDraft = {
@@ -16,35 +28,15 @@ async function registerUser(formData: FormData) {
     firstName: formData.firstName,
     lastName: formData.lastName,
     dateOfBirth: getBirthDate(formData.dateOfBirth),
-    addresses: [
-      {
-        country: getCode(formData.country_shipping) as string,
-        streetName: formData[ShippingFieldsName.STREET_NAME],
-        streetNumber: formData[ShippingFieldsName.STREET_NUMBER],
-        apartment: formData[ShippingFieldsName.APARTMENT],
-        postalCode: formData[ShippingFieldsName.POSTAL_CODE],
-      },
-    ],
+    addresses: [createAddress(formData, 'shipping')],
     shippingAddresses: [0],
     billingAddresses: formData.useAsBillingAddress ? [0] : [1],
     defaultShippingAddress: formData.setAsDefault_shipping ? 0 : undefined,
-    defaultBillingAddress: formData.useAsBillingAddress
-      ? formData.setAsDefault_shipping
-        ? 0
-        : undefined
-      : formData.setAsDefault_billing
-      ? 1
-      : undefined,
+    defaultBillingAddress: getDefaultBillingAddress(formData),
   };
 
   if (!formData.useAsBillingAddress) {
-    customerDraft.addresses?.push({
-      country: getCode(formData.country_billing) as string,
-      streetName: formData[BillingFieldsName.STREET_NAME],
-      streetNumber: formData[BillingFieldsName.STREET_NUMBER],
-      apartment: formData[BillingFieldsName.APARTMENT],
-      postalCode: formData[BillingFieldsName.POSTAL_CODE],
-    });
+    customerDraft.addresses?.push(createAddress(formData, 'billing'));
   }
 
   const client = new Client().clientCredentialsClient;
