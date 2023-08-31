99bf49db2d373e2d3f886c4d94ca119d55fe84fe
Lizaveta <lizadondysh@icloud.com>
2023-08-31 09:05:26 +0300

    fix: add enum for breadcrumbs

--- components/product/breadcrumb.tsx
+++ components/product/breadcrumb.tsx
@@ -8,11 +8,17 @@ interface ProductBreadcrumbProps {
   name: string;
 }
 
+enum BreadcrumbPathNames {
+  CATALOG = 'Catalog',
+  CATEGORY = 'Category',
+  SUBCATEGORY = 'Subcategory',
+}
+
 const ProductBreadcrumb: React.FC<ProductBreadcrumbProps> = ({ name }) => {
   return (
     <Breadcrumb>
       <Breadcrumb.Item>
-        <Link href={Paths.CATALOG}>Catalog</Link>
+        <Link href={Paths.CATALOG}>{BreadcrumbPathNames.CATALOG}</Link>
       </Breadcrumb.Item>
       <Breadcrumb.Item>{name}</Breadcrumb.Item>
     </Breadcrumb>
