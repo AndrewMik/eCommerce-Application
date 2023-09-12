d0773cb7c05f5b0866d705d9a5a7ab70ce8d9ff5
Lizaveta <lizadondysh@icloud.com>
2023-09-13 02:51:12 +0300

    feat: update about us data

--- components/about/data/project-description.ts
+++ components/about/data/project-description.ts
@@ -1,5 +1,15 @@
-const projectDescription = `
-  This is an innovative application built by a dedicated team of professionals. The project aims to ...
-  `;
-
+const projectDescription = {
+  about:
+    "Welcome to our Kiddo Kingdom, the magical eCommerce application for children's toys.\nFrom educational puzzles to adventurous playsets, Kiddo Kingdom offers a wide range of toys that inspire creativity, learning, and endless fun! Our online store provides a seamless shopping experience, catering to kids and parents alike. Our user-friendly platform makes shopping for toys a breeze, offering quality products that inspire creativity, learning, and lots of fun!",
+  technologies: {
+    frontend: ['Frontend', 'TypeScript, React, Next.js'],
+    bundling: ['Bundling', 'Managed by Next.js (using Webpack)'],
+    testing: ['Testing', 'Jest, React Testing Library'],
+    codeQuality: ['Code Quality', 'ESLint, Prettier, Husky'],
+    styles: ['Styles', 'Ant Design'],
+    backend: ['Backend', 'CommerceTools'],
+  },
+  collaboration:
+    'We tried to adhere to the Scrum methodology when working on the project. We chose Jira as the tool for task management.\nEach sprint began with sprint planning, where we clarified the criteria, discussed tasks and divided them into smaller ones. During the sprint, we met for dailies, where each team member shared the status of their tasks. The sprint ended with a retro meeting, where we discussed the results of the sprint and worked on a retro board, where the advantages and disadvantages of the team’s work methodology were listed, as well as solutions to improve it. We used Planning poker to evaluate tasks during the grooming backlog.\nWe also tried to minimize repetitive and routine tasks by setting up automation for Jira, automatic deployment and pre-deployment for each pull request.',
+};
 export default projectDescription;
