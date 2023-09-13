65aef064eda324e45b2c84464b6750c484e1697d
Lizaveta D <lizadondysh@icloud.com>
2023-09-14 01:19:18 +0300

    Feature/EA-141/about-us (#72)
    * feat: implment header and main page navigation to about us page
    
    * feat: add draft data, make layout responsive
    
    * feat: update about us data
    
    * feat: implement about us page
    
    * feat: add tests (to be amended)
    
    * fix: fix technologies block width on sm and md screens
    
    * feat: add more tests

---
+++ components/about/sections/project-description.tsx
@@ -0,0 +1,26 @@
+import { Card } from 'antd';
+
+import FontColors from '../../product/product.data';
+import projectDescription from '../data/project-description';
+
+interface ProjectDescriptionProps {
+  description: typeof projectDescription;
+}
+
+const descriptionTitle = '🧸 About the project';
+
+const ProjectDescription: React.FC<ProjectDescriptionProps> = ({ description }) => {
+  return (
+    <div style={{ flex: 1 }}>
+      <Card title={descriptionTitle} style={{ color: FontColors.BASE, height: '100%' }}>
+        <p
+          dangerouslySetInnerHTML={{
+            __html: description.about.replace(/\n/g, '<br /><br />'),
+          }}
+        ></p>
+      </Card>
+    </div>
+  );
+};
+
+export default ProjectDescription;
