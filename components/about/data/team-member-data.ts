18ad46214790e2291e490f5bafa545ec6710fa6a
Lizaveta D <lizadondysh@icloud.com>
2023-09-13 23:20:21 +0300

    Feature/EA-141/about-us (#67)
    * feat: add branch for sprint 4
    * feat: implment header and main page navigation to about us page
    * feat: add draft data, make layout responsive
    * feat: update about us data
    * feat: implement about us page
    * feat: add tests (to be amended)
    * fix: fix technologies block width on sm and md screens
    * feat: add more tests

---
+++ components/about/data/team-member-data.ts
@@ -0,0 +1,46 @@
+const teamMembers = [
+  {
+    name: 'Alina Nosovets',
+    role: 'React expert, API requests guru',
+    bio: "I'm a Frontend Developer, working with cool stuff like HTML, CSS, JavaScript, TypeScript, React and more.\nI've been learning and getting better at it for a while now. I'm really into the constant changes in this field. I'm pretty sure I'm going the right way in my career and I'm excited to see where it leads.",
+    impact: [
+      'set up project to work with React and Next.js',
+      'implemented app navigation logic',
+      'created Catalog page with enhancements',
+      'worked on Cart page',
+      'advised the team on React issues',
+    ],
+    githubLink: 'https://github.com/alikri',
+    photoURL: 'https://raw.githubusercontent.com/haslie22/kiddo-kingdom-data/data/team-members-images/1.jpeg',
+  },
+  {
+    name: 'Liza Dondysh',
+    role: 'Team Lead, Jira fan',
+    bio: "I'm a UX/UI designer with graphic design background, making cool digital products, ensuring they are intuitive, functional, and aesthetically appealing.\nI master my skills in Front-end development, using my experience in design and giving life to interfaces from Figma, turning them into websites and applications.",
+    impact: [
+      'embedded Jira in the team workflow',
+      'created Registration page',
+      'implemented Product page',
+      'worked on User Profile page',
+      'filled out the data for the project',
+    ],
+    githubLink: 'https://github.com/haslie22',
+    photoURL: 'https://raw.githubusercontent.com/haslie22/kiddo-kingdom-data/data/team-members-images/2.jpeg',
+  },
+  {
+    name: 'Andrew Mikitchuk',
+    role: 'Scrum master, bugs fixer',
+    bio: "I made a career in the field of Talent Acquisition Management, hiring candidates across the Americas and working with diverse countries and regions such as the US, CA, LATAM, EU, CIS, Asia and India.\nI'm also a big programming fan, and now I'm eager to rekindle my programming skills and expand my knowledge in the field.",
+    impact: [
+      'set up repository for the project',
+      'implemented automatic deployment',
+      'created Login page',
+      'worked on User Profile page',
+      'worked on Cart page',
+    ],
+    githubLink: 'https://github.com/AndrewMik',
+    photoURL: 'https://raw.githubusercontent.com/haslie22/kiddo-kingdom-data/data/team-members-images/3.jpeg',
+  },
+];
+
+export default teamMembers;
